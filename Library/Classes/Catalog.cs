797779d94659d6b768588abf7aefe359908495fa
hazem237 <201082@ppu.edu.ps>
2022-11-07 18:43:25 +0200

    Created Project Interfaces

---
+++ Library/Classes/Catalog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Interfaces;
+
+namespace Library.Classes
+{
+    public class Catalog : Search , Manage
+    {
+
+    }
+}
