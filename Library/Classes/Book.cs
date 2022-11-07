4f64e1999c3caaf495238cc9dcffee5a12a41943
hazem237 <201082@ppu.edu.ps>
2022-11-07 18:02:06 +0200

    Created Book class with it's attributes

---
+++ Library/Classes/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Classes
+{
+    internal class Book
+    {
+        private string ISBN { get; set; }
+        private string title { get; set; }
+        private string summary { get; set; }
+        private string publisher { get; set; }
+        private string publication_data { get; set; }
+        private int number_of_pages { get; set; }
+        private string publisher { get; set; }
+
+
+
+    }
+}
