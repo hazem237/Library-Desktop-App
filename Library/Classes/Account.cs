0637d3b65bd94416831008b1aba7d24592759eb1
hazem237 <201082@ppu.edu.ps>
2022-11-07 19:04:34 +0200

    The rest of the necessary classes were built

---
+++ Library/Classes/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Classes
+{
+    internal class Account_State {
+        private string state { get; set; }
+        public Account_State(string state ) { this.state = state; }
+    }
+
+
+    internal class Account
+    {
+        private int Account_number { get; set; }
+        private string History { get; set; }
+        private Date opened { get; set; }
+        private Account_State state { get; set;}
+    }
+}
---
+++ Library/Classes/AccountState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Classes
+{
+    internal class AccountState
+    {
+       
+            public bool Active { get; private set; }
+
+        public bool Frozen { get; private set; }
+        public bool Frozen { get; private set; }
+
+
+        protected AccountState (int id, string name) => (Id, Name) = (id, name);
+
+            public override string ToString() => Name;
+
+            public int CompareTo(object other) => Id.CompareTo(((AccountState)other).Id);
+
+            // Other utility methods ...
+        
+    }
+}
