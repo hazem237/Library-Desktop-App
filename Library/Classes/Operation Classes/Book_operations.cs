e9ed500ac63f1022d1bdebb214d4010412d992d8
hazem237 <201082@ppu.edu.ps>
2022-12-04 12:27:25 +0200

    The files are structured

--- Library/Classes/Operation Classes/Book_operations.cs
+++ Library/Classes/Operation Classes/Book_operations.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Library.Interfaces
+namespace Library.Classes.Operation_Classes
 {
-    public interface Manage
+    internal class Book_operations
     {
-         /* Some Functions */
     }
 }
