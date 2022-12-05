093382d592f87097f0104a9fa8be78d975721773
hazem237 <201082@ppu.edu.ps>
2022-12-05 17:53:10 +0200

    Add Catalog process

---
+++ Library/Classes/Operation Classes/Catalogs_Operation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Classes.Operation_Classes
+{
+    internal class Catalogs_Operation
+    {
+    }
+}
