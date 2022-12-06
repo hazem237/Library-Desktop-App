73e5639a476e2117b447c74f4caaf400e6ec520d
hazem237 <201082@ppu.edu.ps>
2022-12-06 12:30:46 +0200

    Designed Book  Panel

---
+++ Library/Forms/Librarian/Books Panel/Books_Panel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library.Forms.Librarian
+{
+    public partial class Books_Panel : Form
+    {
+        public Books_Panel()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Library/Properties/Resources.Designer.cs
+++ Library/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Library.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Books {
+            get {
+                object obj = ResourceManager.GetObject("Books", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap giammarco_eWpBNXRHfTI_unsplash {
             get {
                 object obj = ResourceManager.GetObject("giammarco-eWpBNXRHfTI-unsplash", resourceCulture);
