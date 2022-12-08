44890dae58e4767102946df7778af0ec83630303
hazem237 <201082@ppu.edu.ps>
2022-12-08 12:54:03 +0200

    Added Account For Patron into Data Base

--- Library/Forms/Admin/Add_Account.cs
+++ Library/Forms/Admin/Add_Account.cs
@@ -22,6 +22,10 @@ namespace Library.Forms.Admin
             {
                 comboBox1.Items.Add(a.Name);
             }
+            foreach (Patron p in ctx.Patrons)
+            {
+                comboBox3.Items.Add(p.Patron_ID);
+            }
         }
 
         private void Add_Account_Load(object sender, EventArgs e)
@@ -31,6 +35,29 @@ namespace Library.Forms.Admin
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Classes.Account a = new Classes.Account();
+            LibraryContext ctx = new LibraryContext();
+            a.History = textBox1.Text;
+            a.Date_opened = textBox2.Text;
+            a.patronID = Convert.ToInt32(comboBox3.SelectedItem.ToString());
+            string key = comboBox1.SelectedItem.ToString();
+            var a2 = ctx.Libraries.Where(Library => Library.Name == key).Single();
+            a.libraryID = a2.Library_ID;
+            switch (comboBox2.SelectedItem.ToString())
+            {
+                case "Frozen":
+                    a.account_State = (Account_State)0;
+                    break;
+                case "Active":
+                    a.account_State = (Account_State)1;
+                    break;
+                case "Closed":
+                    a.account_State = (Account_State)2;
+                    break;
+
+            }
+            ctx.Accounts.Add(a);
+            ctx.SaveChanges();
 
         }
 
